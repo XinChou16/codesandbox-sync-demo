3b5174bac7d6e80fc1fe531d00f1c2b4c4c09d06
Violet <violetsshine@gmail.com>
2021-12-31 21:10:13 +0800

    Hello 2022

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,7 +15,7 @@ export default function HomePage() {
             </div>
         
             <footer>
-                <p>&copy; 2016-2021 &nbsp;<span role="img" aria-label="love">❤️</span></p>
+                <p>&copy; 2016-2022 &nbsp;<span role="img" aria-label="love">❤️</span></p>
                 <div className="beian">
                     <a target="_blank" rel="noopener noreferrer" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011102002518" alt="">
                         <img src={policeImg} alt="" />
